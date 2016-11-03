e32471691b15577268042d17ad377475877a8933
Jacob <froghopperjacob@gmail.com>
2016-11-02 20:15:55 -0400

    woops wrong code

--- jsBot.js
+++ jsBot.js
@@ -1,71 +1,121 @@
-var Discord = require('discord.js');
+var Discord = require("discord.js");
+var fs = require("fs")
+var ytdl = require("ytdl-core");
 var bot = new Discord.Client();
-var owner = 'froghopperjacob';
-var coOwner = 'Musdoy';
-var gConnection = null;
-var sbVoiceId = '234048547073425408';
-var sbTextId = '232587822870036480';
+var voiceChannelid = "242327411247677442"
 var commands = [];
-var muted = [];
+var queues = [];
+var currentVolume = 0.12000000000000006
+var prefix = "!"
 function addCommand(name,description,command,func) {
 	commands.push({Name:name,Desc:description,Calls:command,Function:func});
 }
-addCommand("Ping","Replys with 'Pong'",['ping'],function(message,splitstring) {
-	message.reply("Pong!");
+addCommand("Ping","Replies with Pong",["ping"],function(message,splitString) {
+	message.reply("Pong!")
 });
-addCommand('Help','Replys with all the commands',['help','cmds'],function(message,splitstring) {
+addCommand("Help","Replys with all the commands",["help","cmds"],function(message,splitstring) {
 	for(var i in commands) {
 		message.reply("Command:"+commands[i].Name+" Description:"+commands[i].Desc+" Calls:"+commands[i].Calls);
 	}
 });
-addCommand('Enable tts','enables tts',['enabletts'],function(message,splitstring) {
-	var textchannel = bot.channels.find('id',sbTextId)
-	textchannel.overwritePermissions(everyone, {
-		SEND_TTS_MESSAGES: true
-	})
-});
-addCommand('Disable tts','disables tts',['disabletts'],function(message,splitstring) {
-	var textchannel = bot.channels.find('id',sbTextId)
-	textchannel.overwritePermissions(everyone, {
-		SEND_TTS_MESSAGES: false
-	})
-});
-addCommand('Mute','Makes a person server muted in a voice channel and he cant talk in a text channel',["mute"],function(message,splitstring) {
-	var tableString = splitstring.split(" ");
-	var name = tableString[0];
-	var id = tableString[1];
-	muted.push(name);
-	var serverTxt = bot.channels.find('id',sbTextId);
-	serverTxt.sendTTSMessage(name+" is now muted in the voice channel and in the text channel");
-	var serverVoice = bot.channels.find('id',sbVoiceId);
-	var members = serverVoice.members;
-	console.log(members);
-	console.log(members.get(id))
-});
-addCommand('Code','sends the message into code',['code'],function(message,splitstring) {
-	message.channel.sendCode('lua',splitstring)
-});
-addCommand('Resembalance','that resembalance',['mrbean'],function(message,splitstring) {
-	var sendmessage = bot.channels.find('id',sbTextId);
-	sendmessage.sendMessage('https://cdn.discordapp.com/attachments/2018111111111111111111111111111111111111111169670846824448/201870072837308416/mrbean.jpg');
-});
-addCommand('Blame','The bot says that it blames somebody',['blame'],function(message,splitstring) {
-	var sendtts = bot.channels.find('id',sbTextId);
-	sendtts.sendTTSMessage(message.author.username+" blames "+splitstring);
-});
-bot.on('ready', () => {
-  console.log('Bot Ready!');
-  var channelmsg = bot.channels.find("id",sbTextId);
-	channelmsg.sendTTSMessage('SB BOT Online');
-});
 
-bot.on('message', message => {
-	if (message.content.split("",1) == '/') {
+addCommand("poke","poke ur enemy",["poke"],function(message,splitstring) {
+
+})
+addCommand("play","Plays a song or queues a song",["play"],function(message,splitstring) {
+	var voiceChannel = bot.channels.find("id",voiceChannelid)
+	if (bot.voiceConnections.size == 0) {
+		voiceChannel.join().then(connection => {
+			let dispatcher;
+			play=function(song) {
+				ytdl.getInfo(song,function(err,info) {
+					console.log(song)
+   					console.log(queues.size)
+   					console.log(queues.length)
+					if (song == undefined || song == null || queues.size == 0 || queues.length == 0) {
+						var voiceChannel = bot.channels.find("id",voiceChannelid)
+						message.channel.sendMessage("Queue is finished")
+						voiceChannel.leave()
+						console.log('nil')
+						return
+					}
+					if (err) return message.channel.sendMessage('Invalid Youtube Link: ' + err)
+	   				message.channel.sendMessage("Now playing "+info.title+" and was queued by "+message.author.username)
+					const stream = ytdl(song, {filter : "audioonly"});
+	   				const dispatcher = connection.playStream(stream)
+	   				dispatcher.setVolume(currentVolume)
+	   				let collector = message.channel.createCollector(m => m)
+	   				collector.on("message", m => {
+						if (m.content.startsWith(prefix + "pause")) {
+							message.channel.sendMessage("The song,"+info.title+", is now paused").then(() => {dispatcher.pause();});
+						} else if (m.content.startsWith(prefix + "resume")){
+							message.channel.sendMessage("The song,"+info.title+", is now resumed").then(() => {dispatcher.resume();});
+						} else if (m.content.startsWith(prefix + "skip")){
+							message.channel.sendMessage("Skipped the song,"+info.title).then(() => {dispatcher.end();});
+						} else if (m.content.startsWith(prefix+"volume+")){
+							if (Math.round(dispatcher.volume*50) >= 100) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+							dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split("+").length-1)))/50,2));
+							currentVolume = Math.min((dispatcher.volume*50 + (2*(m.content.split("+").length-1)))/50,2)
+							message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+						} else if (m.content.startsWith(prefix+"volume-")){
+							if (Math.round(dispatcher.volume*50) <= 0) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+							dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split("-").length-1)))/50,0));
+							currentVolume = Math.max((dispatcher.volume*50 - (2*(m.content.split("-").length-1)))/50,0)
+							message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+						} else if (m.content.startsWith(prefix + "time")){
+							message.channel.sendMessage(`Current video time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? "0"+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+						} else if(m.content.startsWith(prefix + "clear")){
+							message.channel.sendMessage("Cleared the queue").then(() => {queues.length = 0})
+						}
+	   				})
+	   				dispatcher.on("end", () => {
+	   					collector.stop();
+	   					queues.shift()
+	   					play(queues[0])
+	   					console.log(queues[0])
+	   					console.log(queues.size)
+	   					console.log(queues.length)
+	   					if(queues[0] == undefined || queues[0] == null || queues.size == 0 || queues.length == 0) {
+							var voiceChannel = bot.channels.find("id",voiceChannelid)
+							console.log('nil')
+							message.channel.sendMessage("Queue is finished")
+							voiceChannel.leave()
+							return
+	   					}
+	   				})
+	   				dispatcher.on("error", error => {
+	   					return message.channel.sendMessage("error: "+error).then(() => {
+	   						collector.stop()
+	   						queues.shift()
+	   						play(queues[0])
+	   					})
+	   				})
+				})
+			}
+			play(splitstring)
+			queues.push(splitstring)
+		}).catch(console.error)
+	} else if (bot.voiceConnections.size != 0) {
+		queues.push(splitstring)
+		ytdl.getInfo(splitstring,function(err,info) {
+			message.channel.sendMessage("Queued "+info.title+" in #"+queues.length+" queue spot by "+message.author.username)
+		})
+	}
+})
+addCommand("setId","sets the voiceChannel in which the bot will join",["setid"],function(message,splitstring) {
+	voiceChannelid = splitstring
+})
+addCommand("forceLeave","force leaves the bot",["forceleave"],function(message,splitstring) {
+	var voiceChannel = bot.channels.find("id",voiceChannelid)
+	voiceChannel.leave()
+})
+bot.on("message", message => {
+	if (message.content.split("",1) == prefix) {
 		for (var i in commands) {
 			for (var a in commands[i].Calls) {
 				var checkString = message.content.split(" ",1)[0];
 				var length = checkString.length;
-				if ("/"+commands[i].Calls[a] == checkString) {
+				if (prefix+commands[i].Calls[a] == checkString) {
 					try {
 						var splitString = message.content.slice(length+1);
 						commands[i].Function(message,splitString);
@@ -78,4 +128,10 @@ bot.on('message', message => {
 		}
 	}
 });
-bot.login("MjMyNTgyMTk4MTkwOTMxOTY4.CtREUA.6gwUtoslC_tRBEUMUGIoDn1gmA4");
+bot.on("ready", () => {
+  console.log("Bot Ready!");
+  var channelmsg = bot.channels.find("id","234694901386838017");
+  channelmsg.sendMessage("Hologram bot Online");
+  bot.user.setGame("with Music")
+});
+bot.login("MjQyMzQ0MDAyNDU4ODc3OTUy.CvfGew.BC1NlQDxMarglXiUhPbtbbovJ0E");
